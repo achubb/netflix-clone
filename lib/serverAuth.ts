ea37fffa29b48aa3d87e2b4e8d93572f9acf3013
Andrew Chubb <achubb@fastmail.fm>
2023-05-08 15:35:55 +0100

    Set up protected routes and added current user hook

---
+++ lib/serverAuth.ts
@@ -0,0 +1,27 @@
+import  { NextApiRequest } from 'next';
+import { getSession } from 'next-auth/react';
+
+import prismadb from '@/lib/prismadb'
+
+const serverAuth = async (req: NextApiRequest) => {
+    const session = await getSession({ req });
+
+    // If session or user or email doens't exist
+    if (!session?.user?.email) {
+        throw new Error('Not signed in');
+    }
+
+    const currentUser = await prismadb.user.findUnique({
+        where: {
+            email: session.user.email,
+        }
+    });
+
+    if (!currentUser) {
+        throw new Error('Not signed in');
+    }
+
+    return { currentUser }
+}
+
+export default serverAuth;
