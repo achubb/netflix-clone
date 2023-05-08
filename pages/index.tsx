36e4c703a8d40499422ee9087c2977567310ee2f
Andrew Chubb <achubb@fastmail.fm>
2023-05-08 19:04:30 +0100

    Added nav and mobile nav

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import useCurrentUser from "@/hooks/useCurrentUser";
+import Navbar from "@/components/Navbar";
 import { NextPageContext } from "next"
-import { getSession, signOut } from "next-auth/react"
+import { getSession } from "next-auth/react"
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -20,13 +20,9 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
-
   return (
     <>
-      <h1 className="text-green-500">Netflix Clone</h1>
-      <p>Logged in as : {user?.name}</p>
-      <button className="h-10 w-full bg-white" onClick={() => signOut()}>Logout</button>
+      <Navbar></Navbar>
     </>
   )
 }
